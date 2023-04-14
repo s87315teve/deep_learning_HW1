178879738bdf5f4d8b31fb494674c7aeb93fee24
s87315teve <37323304+s87315teve@users.noreply.github.com>
2023-04-14 23:34:54 +0800

    Update audio_transfer.py

--- audio_transfer.py
+++ audio_transfer.py
@@ -4,7 +4,7 @@ import datetime
 import subprocess
 import csv
 
-'''
+
 origin_folder="train_origin"
 new_folder="train"
 #train_origin
@@ -13,11 +13,11 @@ if not folder_exist:
     os.mkdir("train")
 file_list=[f for f in os.listdir(origin_folder)]
 for filename in file_list:
-    #print("{}\\{} -r 16000 -e signed-integer -b 16 {}\\{}".format(origin_folder, filename, new_folder, filename))
+    #print("{}\\{} -r 16000 -e signed-integer -b 16 {}/{}".format(origin_folder, filename, new_folder, filename))
     subprocess.getstatusoutput("sox {}\\{} -r 16000 -e signed-integer -b 16 {}\\{}".format(origin_folder, filename, new_folder, filename))
 
 print("train folder finished")
-'''
+
 
 origin_folder="test_origin"
 new_folder="test"
