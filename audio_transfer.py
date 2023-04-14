a057ad045f816d9772f80bcba1bedf69b2c231e6
s87315teve <37323304+s87315teve@users.noreply.github.com>
2023-04-14 23:39:04 +0800

    Update audio_transfer.py

--- audio_transfer.py
+++ audio_transfer.py
@@ -13,8 +13,8 @@ if not folder_exist:
     os.mkdir("train")
 file_list=[f for f in os.listdir(origin_folder)]
 for filename in file_list:
-    #print("{}\\{} -r 16000 -e signed-integer -b 16 {}/{}".format(origin_folder, filename, new_folder, filename))
-    subprocess.getstatusoutput("sox {}\\{} -r 16000 -e signed-integer -b 16 {}\\{}".format(origin_folder, filename, new_folder, filename))
+    #print("{}/{} -r 16000 -e signed-integer -b 16 {}/{}".format(origin_folder, filename, new_folder, filename))
+    subprocess.getstatusoutput("sox {}/{} -r 16000 -e signed-integer -b 16 {}/{}".format(origin_folder, filename, new_folder, filename))
 
 print("train folder finished")
 
@@ -27,5 +27,5 @@ if not folder_exist:
     os.mkdir("test")
 file_list=[f for f in os.listdir(origin_folder)]
 for filename in file_list:
-    subprocess.getstatusoutput("sox {}\\{} -r 16000 -e signed-integer -b 16 {}\\{}".format(origin_folder, filename, new_folder, filename))
+    subprocess.getstatusoutput("sox {}/{} -r 16000 -e signed-integer -b 16 {}/{}".format(origin_folder, filename, new_folder, filename))
 print("test folder finished")
